966a36d00b4a1f92205a33151c736264b78d6c58
Jey Antonio Paniagua Garcia <jeypaniagua@gmail.com>
2022-11-15 15:51:33 -0600

    Revert "se crea el respositorio y ademas se establece que la respuesta de la api la guarde en una clase"
    This reverts commit 9ce15821a0cab4bf22de34a3c935221173391990.

--- Donut/app/src/main/java/com/codein/donut/data/NotasProvider.kt
+++
@@ -1,9 +0,0 @@
-package com.codein.donut.data
-
-import com.codein.donut.data.model.NotasResponse
-
-class NotasProvider {
-    companion object {
-        var notas : NotasResponse? = null
-    }
-}
