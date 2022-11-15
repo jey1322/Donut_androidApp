c43810a5ac2be5f265d026b71aa75487508e50bf
Jey Antonio Paniagua Garcia <jeypaniagua@gmail.com>
2022-11-14 18:38:12 -0600

    configuramos el binding para ambas pantallas

--- Donut/app/src/main/java/com/codein/donut/Home.kt
+++ Donut/app/src/main/java/com/codein/donut/Home.kt
@@ -2,10 +2,14 @@ package com.codein.donut
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codein.donut.databinding.ActivityHomeBinding
 
 class Home : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
