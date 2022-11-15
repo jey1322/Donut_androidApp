88a53b435b7eb77e7cf095f4849142f849a0c987
Jey Antonio Paniagua Garcia <jeypaniagua@gmail.com>
2022-11-14 18:38:26 -0600

    configuramos el binding para ambas pantallas

--- Donut/app/src/main/java/com/codein/donut/MainActivity.kt
+++ Donut/app/src/main/java/com/codein/donut/MainActivity.kt
@@ -2,11 +2,15 @@ package com.codein.donut
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codein.donut.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_Donut)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
