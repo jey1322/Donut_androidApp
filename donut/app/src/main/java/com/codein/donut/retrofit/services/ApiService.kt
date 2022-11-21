9e7f15f561097bf3a5620be63cb51b70181c2057
Jey Antonio Paniagua Garcia <jeypaniagua@gmail.com>
2022-11-20 22:09:06 -0600

    apiclient y apiservice

---
+++ donut/app/src/main/java/com/codein/donut/retrofit/services/ApiService.kt
@@ -0,0 +1,13 @@
+package com.codein.donut.retrofit.services
+
+import com.codein.donut.retrofit.LoginRequest
+import com.codein.donut.retrofit.LoginResponse
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface ApiService {
+
+    @POST(Urls.LOGIN_URL)
+    suspend fun login(@Body request: LoginRequest): Call<LoginResponse>
+}
---
+++ donut/app/src/main/java/com/codein/donut/retrofit/services/Urls.kt
@@ -0,0 +1,6 @@
+package com.codein.donut.retrofit.services
+
+object Urls {
+    const val BASE_URL = "http://cb-develop.tk/"
+    const val LOGIN_URL = "api/v1/result-grades"
+}
