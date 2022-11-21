cc4b1077135a3889bb8d4812bdea96184187a696
Jey Antonio Paniagua Garcia <jeypaniagua@gmail.com>
2022-11-20 23:07:19 -0600

    Login funcionando

--- donut/app/src/main/java/com/codein/donut/MainActivity.kt
+++ donut/app/src/main/java/com/codein/donut/MainActivity.kt
@@ -2,16 +2,12 @@ package com.codein.donut
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.text.InputType
 import android.widget.Toast
 import com.codein.donut.databinding.ActivityMainBinding
 import com.codein.donut.retrofit.LoginRequest
 import com.codein.donut.retrofit.LoginResponse
 import com.codein.donut.retrofit.services.ApiClient
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -29,31 +25,77 @@ class MainActivity : AppCompatActivity() {
         apiClient = ApiClient()
 
         binding.btnIngresar.setOnClickListener {
-                apiClient.getApiService(this).login(
-                    LoginRequest(
-                        id = "16-01447-0",
-                        password = "NAFUWP",
-                        year = "2021"
-                    )
-                )
-                    .enqueue(object : Callback<LoginResponse>{
 
-                        override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
-                            Toast.makeText(this@MainActivity, "Error al conectar", Toast.LENGTH_SHORT).show()
-                        }
+            binding.etCarnet.error = null
+            binding.etPin.error = null
+            binding.etAnio.error = null
 
-                        override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>
-                        ) {
-                            val resp = response.body()
-                            if (resp?.student?.name != null) {
-                                Toast.makeText(this@MainActivity, "Bienvenido...", Toast.LENGTH_SHORT).show()
-                            }
-                            else {
-                                Toast.makeText(this@MainActivity, "Error al iniciar sesión", Toast.LENGTH_SHORT).show()
-                            }
-                        }
+            val id = binding.etCarnet.text.toString().trim()
+            val pin = binding.etPin.text.toString().trim()
+            var year = binding.etAnio.text.toString().trim()
 
-                    })
+            if(id.isEmpty())
+            {
+                Toast.makeText(this, "Ingrese su carnet", Toast.LENGTH_SHORT).show()
+            }
+            else
+            {
+                if (pin.isEmpty())
+                {
+                    Toast.makeText(this, "Ingrese su pin", Toast.LENGTH_SHORT).show()
+                }
+                else
+                {
+                    if (year.isEmpty())
+                    {
+                        year = java.util.Calendar.getInstance().get(java.util.Calendar.YEAR).toString()
+                        binding.etAnio.setText(year)
+                    }
+                    else{
+
+                        binding.btnIngresar.isEnabled = false
+                        binding.btnIngresar.background = getDrawable(R.drawable.buttonproc)
+                        binding.btnIngresar.text = "solicitando datos..."
+
+                        apiClient.getApiService(this).login(LoginRequest(
+                            id = id,
+                            password = pin,
+                            year = year
+                        ))
+                            .enqueue(object : Callback<LoginResponse>{
+
+                                override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                                    Toast.makeText(this@MainActivity, "Error al conectar", Toast.LENGTH_SHORT).show()
+                                    binding.btnIngresar.isEnabled = true
+                                    binding.btnIngresar.background = getDrawable(R.drawable.btn)
+                                    binding.btnIngresar.text = "Ver notas"
+                                }
+
+                                override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>
+                                ) {
+                                    val resp = response.body()
+                                    if (resp?.student?.name != null) {
+                                        Toast.makeText(this@MainActivity, "Bienvenido...", Toast.LENGTH_SHORT).show()
+                                    }
+                                    else {
+                                        binding.etAnio.error = "este es el año a buscar?"
+                                        //pintar el contenido de etpass
+                                        binding.etPin.error = "PIN es correcto?"
+                                        binding.etPin.inputType = InputType.TYPE_CLASS_TEXT
+                                        binding.etCarnet.error = "Carnet es correcto?"
+                                        Toast.makeText(this@MainActivity, "Error: Algo salió mal", Toast.LENGTH_SHORT).show()
+                                        binding.btnIngresar.isEnabled = true
+                                        binding.btnIngresar.background = getDrawable(R.drawable.btn)
+                                        binding.btnIngresar.text = "Ver notas"
+                                    }
+                                }
+
+                            })
+                    }
+                }
+            }
+
+
         }
 
     }
