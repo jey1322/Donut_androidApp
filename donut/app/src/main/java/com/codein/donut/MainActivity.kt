b392100fe628acd990e941c2960a838c94caa688
Jey Antonio Paniagua Garcia <jeypaniagua@gmail.com>
2022-11-23 19:16:25 -0600

    agregamos splashscreen

--- donut/app/src/main/java/com/codein/donut/MainActivity.kt
+++ donut/app/src/main/java/com/codein/donut/MainActivity.kt
@@ -21,6 +21,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_Donut)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
