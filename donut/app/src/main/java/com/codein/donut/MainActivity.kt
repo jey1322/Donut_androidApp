d10c17900cdfcf6f4137ef5ec78fe3ac6c9ccb02
Jey Antonio Paniagua Garcia <jeypaniagua@gmail.com>
2023-05-22 22:25:40 -0600

    identamos y corregimos main

--- donut/app/src/main/java/com/codein/donut/MainActivity.kt
+++ donut/app/src/main/java/com/codein/donut/MainActivity.kt
@@ -17,17 +17,15 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var apiClient: ApiClient
     private lateinit var sessionManager: SessionManager
-
     private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_Donut)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         apiClient = ApiClient()
         sessionManager = SessionManager(this)
 
@@ -37,105 +35,69 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
-
         binding.clear.setOnClickListener {
             binding.banner.visibility = View.GONE
         }
 
         binding.btnIngresar.setOnClickListener {
-
-            binding.etCarnet.error = null
-            binding.etPin.error = null
-            binding.etAnio.error = null
-
             val id = binding.etCarnet.text.toString().trim()
             val pin = binding.etPin.text.toString().trim()
             var year = binding.etAnio.text.toString().trim()
 
-            if(id.isEmpty())
-            {
+            if(id.isEmpty()) {
                 Toast.makeText(this, "Ingrese su carnet", Toast.LENGTH_SHORT).show()
+            } else if (pin.isEmpty()) {
+                Toast.makeText(this, "Ingrese su pin", Toast.LENGTH_SHORT).show()
+            } else if (year.isEmpty()) {
+                year = java.util.Calendar.getInstance().get(java.util.Calendar.YEAR).toString()
+                binding.etAnio.setText(year)
+            } else{
+                binding.btnIngresar.isEnabled = false
+                binding.btnIngresar.background = getDrawable(R.drawable.buttonproc)
+                binding.btnIngresar.text = "solicitando datos..."
+
+                sessionManager.savelid(id)
+                sessionManager.savelpass(pin)
+                sessionManager.savelyear(year)
+
+                apiClient.getApiService(this).login(LoginRequest(id = id, password = pin, year = year))
+                    .enqueue(object : Callback<LoginResponse>{
+                        override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                            Toast.makeText(this@MainActivity, "Error en conexión, inténtelo de nuevo", Toast.LENGTH_SHORT).show()
+                            binding.btnIngresar.isEnabled = true
+                            binding.btnIngresar.background = getDrawable(R.drawable.btn)
+                            binding.btnIngresar.text = "Ver notas"
+                        }
+                        override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
+                            binding.banner.visibility = View.GONE
+                            val resp = response.body()
+                            if (resp?.student?.name != null) {
+                                sessionManager.saveName(resp.student.name)
+                                sessionManager.saveCareer(resp.student.career)
+                                sessionManager.saveId(resp.student.id)
+                                sessionManager.saveYear(resp.student.year)
+                                sessionManager.saveFaculty(resp.student.faculty)
+                                sessionManager.saveCycleYear(resp.student.cycle_year)
+                                sessionManager.saveAverage(resp.student.average)
+                                Toast.makeText(this@MainActivity, "Bienvenido...", Toast.LENGTH_SHORT).show()
+                                val intent = Intent(this@MainActivity, Home::class.java)
+                                startActivity(intent)
+                                finish()
+                            } else {
+                                binding.banner.visibility = View.VISIBLE
+                                binding.etAnio.error = "este es el año a buscar?"
+                                binding.etPin.error = "PIN es correcto?"
+                                binding.etPin.inputType = InputType.TYPE_CLASS_TEXT
+                                binding.etCarnet.error = "Carnet es correcto?"
+                                Toast.makeText(this@MainActivity, "Error: Algo salió mal", Toast.LENGTH_SHORT).show()
+                                binding.btnIngresar.isEnabled = true
+                                binding.btnIngresar.background = getDrawable(R.drawable.btn)
+                                binding.btnIngresar.text = "Ver notas"
+                            }
+                        }
+
+                    })
             }
-            else
-            {
-                if (pin.isEmpty())
-                {
-                    Toast.makeText(this, "Ingrese su pin", Toast.LENGTH_SHORT).show()
-                }
-                else
-                {
-                    if (year.isEmpty())
-                    {
-                        year = java.util.Calendar.getInstance().get(java.util.Calendar.YEAR).toString()
-                        binding.etAnio.setText(year)
-                    }
-                    else{
-
-                        binding.btnIngresar.isEnabled = false
-                        binding.btnIngresar.background = getDrawable(R.drawable.buttonproc)
-                        binding.btnIngresar.text = "solicitando datos..."
-
-                        sessionManager.savelid(id)
-                        sessionManager.savelpass(pin)
-                        sessionManager.savelyear(year)
-
-                        //recuerda que el erro de okhttp tal y tal cosa, intenta probar enviado los valores estaticos
-                        apiClient.getApiService(this).login(LoginRequest(
-                            id = id,
-                            password = pin,
-                            year = year
-                        ))
-                            .enqueue(object : Callback<LoginResponse>{
-
-                                override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
-                                    Toast.makeText(this@MainActivity, "Error en conexión, inténtelo de nuevo", Toast.LENGTH_SHORT).show()
-                                    binding.btnIngresar.isEnabled = true
-                                    binding.btnIngresar.background = getDrawable(R.drawable.btn)
-                                    binding.btnIngresar.text = "Ver notas"
-                                }
-
-                                override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>
-                                ) {
-                                    binding.banner.visibility = View.GONE
-                                    val resp = response.body()
-                                    if (resp?.student?.name != null) {
-
-                                        sessionManager.saveName(resp.student.name)
-                                        sessionManager.saveCareer(resp.student.career)
-                                        sessionManager.saveId(resp.student.id)
-                                        sessionManager.saveYear(resp.student.year)
-                                        sessionManager.saveFaculty(resp.student.faculty)
-                                        sessionManager.saveCycleYear(resp.student.cycle_year)
-                                        sessionManager.saveAverage(resp.student.average)
-
-                                        Toast.makeText(this@MainActivity, "Bienvenido...", Toast.LENGTH_SHORT).show()
-                                        val intent = Intent(this@MainActivity, Home::class.java)
-                                        startActivity(intent)
-                                        finish()
-                                    }
-                                    else {
-
-                                        binding.banner.visibility = View.VISIBLE
-
-                                        binding.etAnio.error = "este es el año a buscar?"
-                                        //pintar el contenido de etpass
-                                        binding.etPin.error = "PIN es correcto?"
-                                        binding.etPin.inputType = InputType.TYPE_CLASS_TEXT
-                                        binding.etCarnet.error = "Carnet es correcto?"
-                                        Toast.makeText(this@MainActivity, "Error: Algo salió mal", Toast.LENGTH_SHORT).show()
-                                        binding.btnIngresar.isEnabled = true
-                                        binding.btnIngresar.background = getDrawable(R.drawable.btn)
-                                        binding.btnIngresar.text = "Ver notas"
-                                    }
-                                }
-
-                            })
-                    }
-                }
-            }
-
-
         }
-
     }
 }
