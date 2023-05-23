1ce73a7a329eacb8e92d221cdb6cf2410324c5b5
Jey Antonio Paniagua Garcia <jeypaniagua@gmail.com>
2023-05-22 22:03:18 -0600

    actualizamos gradle y actualizamos url

--- donut/app/src/main/java/com/codein/donut/retrofit/services/Urls.kt
+++ donut/app/src/main/java/com/codein/donut/retrofit/services/Urls.kt
@@ -2,5 +2,5 @@ package com.codein.donut.retrofit.services
 
 object Urls {
     const val BASE_URL = "http://cb-develop.tk/"
-    const val LOGIN_URL = "api/v1/result-grades"
+    const val LOGIN_URL = "api/test"
 }
